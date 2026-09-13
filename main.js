d0f83480562fb6ac3487c7d86e7541c0ef9fbd11
fatma hamada <fstmahamada@gmail.com>
2026-09-13 10:05:49 +0300

    website using front page

--- main.js
+++ main.js
@@ -20,19 +20,28 @@ LiveReloadServer.server.once("connect", () =>{
 });
 
 // 
-
 app.set('view engine','ejs');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 
-app.get('/', (req, res) => {
-    Mydata.find()
-    .then((result)=>{    res.render("index",{mytitle:"home page",array:result});})
-    .catch((err)=>{console.log(err)});
 
+
+app.get('/', (req, res) => {
+    res.render("index",{});
 });
 
+app.get('/create.html', (req, res) => {
+    res.render("create",{});
+});
+
+app.get('/update.html', (req, res) => {
+    res.render("update",{});
+});
+
+app.get('/show.html', (req, res) => {
+    res.render("show",{});
+});
 
 mongoose.connect('mongodb://fatma:X9cRsP.vTD4XKp5@ac-eb5lemd-shard-00-00.uroy8ia.mongodb.net:27017,ac-eb5lemd-shard-00-01.uroy8ia.mongodb.net:27017,ac-eb5lemd-shard-00-02.uroy8ia.mongodb.net:27017/all-data?ssl=true&replicaSet=atlas-yrwkbl-shard-0&authSource=admin&appName=Cluster0')
 .then(()=>{app.listen(port,()=>{
