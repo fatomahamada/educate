b1ea62b55ac39939ed04afb28d1eaf6d63903314
fatma hamada <fstmahamada@gmail.com>
2026-09-13 21:12:22 +0300

    ejs component

--- main.js
+++ main.js
@@ -43,6 +43,14 @@ app.get('/show.html', (req, res) => {
     res.render("show",{});
 });
 
+app.get('/about.html', (req, res) => {
+    res.render("about",{});
+});
+
+app.get('/contact.html', (req, res) => {
+    res.render("contactus",{});
+});
+
 mongoose.connect('mongodb://fatma:X9cRsP.vTD4XKp5@ac-eb5lemd-shard-00-00.uroy8ia.mongodb.net:27017,ac-eb5lemd-shard-00-01.uroy8ia.mongodb.net:27017,ac-eb5lemd-shard-00-02.uroy8ia.mongodb.net:27017/all-data?ssl=true&replicaSet=atlas-yrwkbl-shard-0&authSource=admin&appName=Cluster0')
 .then(()=>{app.listen(port,()=>{
     console.log('http://localhost: ${port}');
